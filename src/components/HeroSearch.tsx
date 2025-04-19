050334294741b0c1d6e1dd3f1839fce76f0c151e
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 <vaidikbhesaniya1212@gmail.com>
2025-04-19 23:29:52 +0530

    first commit

--- src/components/HeroSearch.tsx
+++ src/components/HeroSearch.tsx
@@ -1,23 +1,50 @@
 "use client";
 
-import { Dispatch, SetStateAction } from "react";
+import { Dispatch, SetStateAction, useState } from "react";
+import { PlaceholdersAndVanishInput as AceternityInput } from "./ui/placeholders-and-vanish-input";
 
-export interface PlaceholdersAndVanishInputProps {
+function HeroSearch() {
+  const [search, setSearch] = useState("");
+
+  return (
+    <div className="bg-white backdrop-blur-lg p-6 md:p-12 py-10 md:py-16 rounded-2xl shadow-lg w-full">
+      <h2 className="text-xl md:text-2xl font-bold text-gray-900">Where</h2>
+      <p className="text-sm md:text-base">Enter your destination</p>
+      <PlaceholdersAndVanishInputDemo search={search} setSearch={setSearch} />
+    </div>
+  );
+}
+
+export default HeroSearch;
+
+interface PlaceholdersAndVanishInputProps {
   search: string;
   setSearch: Dispatch<SetStateAction<string>>;
 }
 
-export function HeroSearch({
-  search,
-  setSearch,
-}: PlaceholdersAndVanishInputProps) {
+function PlaceholdersAndVanishInputDemo({ search, setSearch }: PlaceholdersAndVanishInputProps) {
+  const placeholders = [
+    "Where's the best street food in Bangkok?",
+    "Which rooftop bar in New York has the best view?",
+    "Take me to a breathtaking viewpoint in Santorini!",
+    "Find a peaceful beach getaway near Goa.",
+    "What’s a must-visit historical site in Rome?",
+    search
+  ];
+
+  const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    setSearch(e.target.value);
+  };
+  const onSubmit = (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+  };
   return (
-    <input
-      type="text"
-      value={search}
-      onChange={(e) => setSearch(e.target.value)}
-      placeholder="Type your destination..."
-      className="w-full px-4 py-2 border border-gray-300 rounded-lg focus:outline-none focus:ring-2 focus:ring-blue-500 transition-all"
-    />
+    <div className="flex flex-col justify-center items-center">
+      <AceternityInput
+        placeholders={placeholders}
+        onChange={handleChange}
+        onSubmit={onSubmit}
+      />
+    </div>
   );
 }

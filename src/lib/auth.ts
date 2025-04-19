3e8c82c2071bae12a2b0865dea33d20ac8c373d6
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 <vaidikbhesaniya1212@gmail.com>
2025-04-19 23:38:09 +0530

    first commit

--- src/lib/auth.ts
+++ src/lib/auth.ts
@@ -10,12 +10,12 @@ export interface UserJWT {
 const SECRET_KEY = new TextEncoder().encode(process.env.SECRET_KEY || "");
 
 // Generate a JWT token
-export const generateToken = async (payload: Record<string, object>, expiresIn: string = "1h") => {
+export const generateToken = async (payload: Record<string, string>, expiresIn: string = "1h") => {
   return await new SignJWT(payload).setProtectedHeader({ alg: "HS256" }).setExpirationTime(expiresIn).sign(SECRET_KEY);
 };
 
 // Verify a JWT token
 export const verifyToken = async (token: string) => {
   const { payload } = await jwtVerify(token, SECRET_KEY);
-  return payload as Record<string, object>;
+  return payload as Record<string, string>;
 };
